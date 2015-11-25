1233f61bb5f3589a62aa68392138ab11d31ec9c5
sathis <sathis@genwi.com>
2015-11-25 16:16:24 +0530

    Initial version. Streaming and Fetch support

---
+++ index.js
@@ -0,0 +1,85 @@
+'use strict';
+
+var async = require('async');
+var url = require('url');
+var request = require('request');
+var ntlm = require('./lib/ntlm');
+var _ = require('underscore');
+
+ var defaults = {
+ 	workstation: '',
+ 	domain: ''
+ }
+
+ function makeRequest(stream, options, callback){
+	
+ 	options = _.extend(defaults, options);
+	var reqOptions = _.extend({}, options.request);
+
+	async.waterfall([
+		function ($){
+			var type1msg = ntlm.createType1Message(options);
+
+			// build type1 request:
+			var type1options = {
+				headers:{
+					'Connection' : 'keep-alive',
+					'Authorization': type1msg
+				},
+				timeout: options.timeout || 0,
+				forever: true			
+			};
+			type1options.url = options.url;
+
+			// add timeout option:
+			if(reqOptions.timeout) 
+				type1options.timeout = reqOptions.timeout;
+
+			// send type1 message to server:
+			request(type1options, $);
+		},
+
+		function (res, body, $){
+			if(!res.headers['www-authenticate'])
+				return $(new Error('www-authenticate not found on response of second request'));
+
+			// parse type2 message from server:
+			var type2msg = ntlm.parseType2Message(res.headers['www-authenticate']);
+
+			// create type3 message:
+			var type3msg = ntlm.createType3Message(type2msg, options);
+
+			// build type3 request:
+			var type3options = {
+				headers: {
+					'Connection': 'Close',
+					'Authorization': type3msg
+				},
+				followRedirect: false,
+				forever: true
+			};
+			type3options.url = options.url;
+
+			// pass along other options:
+			type3options.headers = _.extend(type3options.headers, reqOptions.headers);
+			type3options = _.extend(type3options, _.omit(reqOptions, 'headers'));
+
+			// send type3 message to server:
+			if (stream) {
+				$(request(type3options));
+			} else {
+				request(type3options, $);
+			}
+		}
+	], callback);
+}
+
+exports.stream = function(options, callback) {
+	makeRequest(true, options, callback);
+}
+
+exports.fetch = function(options, callback) {
+	makeRequest(false, options, callback);
+}
+
+exports.ntlm = ntlm; //if you want to use the NTML functions yourself
---
+++ test/index.js
@@ -0,0 +1,24 @@
+var ntlm = require('../lib/httpntlm');
+
+var options = {
+	url: <URL>,
+	username: <Username>,
+	password: <Password>,
+	request: {
+		headers: {
+			'Accept': 'application/json'
+		}
+	}
+};
+
+ntlm.fetch(options, function(err, resp, body) {
+	console.log(err, body);
+}, function(err) {
+	 console.log('Error is : ', err);
+});
+
+ntlm.stream(options, function(request) {
+	//request.pipe(process.stdout);
+}, function(err) {
+	 console.log('Error is : ', err);
+});
