973085bbd15d7a7726f54627ebe8fe47863ee3ed
Mueters <jordi.mueters@accenture.com>
2018-12-27 13:15:23 +0100

    Dropped async and underscore dependencies Use of const/let in favor of var (ECMAScript 6) Fixed a bug where no callback is used in case of an error in parseType2Message

--- index.js
+++ index.js
@@ -1,86 +1,85 @@
 'use strict';
 
-var async = require('async');
-var url = require('url');
-var request = require('request');
-var ntlm = require('./lib/ntlm');
-var _ = require('underscore');
+const request = require('request');
+const ntlm = require('./lib/ntlm');
 
- var defaults = {
- 	workstation: '',
- 	domain: ''
- }
+function makeRequest(stream, options, callback) {
+    options = {
+        url: options.url || '',
+        username: options.username || '',
+        password: options.password || '',
+        workstation: options.workstation || '',
+        domain: options.domain || '',
+        request: options.request || {}
+    };
 
- function makeRequest(stream, options, callback){
-	
- 	options = _.extend(defaults, options);
-	var reqOptions = _.extend({}, options.request);
-	reqOptions.url = reqOptions.url || options.url;
+    const reqOptions = options.request;
+    reqOptions.url = reqOptions.url || options.url;
 
-	async.waterfall([
-		function ($){
-			var type1msg = ntlm.createType1Message(options);
+    function makeType1Request($) {
+        const type1msg = ntlm.createType1Message(options);
 
-			// build type1 request:
-			var type1options = {
-				headers:{
-					'Connection' : 'keep-alive',
-					'Authorization': type1msg
-				},
-				timeout: options.timeout || 0,
-				forever: true			
-			};
-			type1options.url = reqOptions.url;
+        // build type1 request:
+        const type1options = Object.assign(reqOptions, {
+            followRedirect: false,
+            forever: true,
+            headers: Object.assign(reqOptions.headers, {
+                'Connection': 'keep-alive',
+                'Authorization': type1msg
+            })
+        });
 
-			// add timeout option:
-			if(reqOptions.timeout) 
-				type1options.timeout = reqOptions.timeout;
+        // send type1 message to server:
+        request(type1options, $);
+    }
 
-			// send type1 message to server:
-			request(type1options, $);
-		},
+    function makeType3Request(authenticateHeader, $) {
+        // parse type2 message from server:
+        const type2msg = ntlm.parseType2Message(authenticateHeader, $);
+        if (!type2msg) {
+            return;
+        }
 
-		function (res, body, $){
-			if(!res.headers['www-authenticate'])
-				return $(new Error('www-authenticate not found on response of second request'));
+        // create type3 message:
+        const type3msg = ntlm.createType3Message(type2msg, options);
 
-			// parse type2 message from server:
-			var type2msg = ntlm.parseType2Message(res.headers['www-authenticate']);
+        // build type3 request:
+        const type3options = Object.assign(reqOptions, {
+            followRedirect: false,
+            forever: true,
+            headers: Object.assign(reqOptions.headers, {
+                'Connection': 'Close',
+                'Authorization': type3msg
+            })
+        });
 
-			// create type3 message:
-			var type3msg = ntlm.createType3Message(type2msg, options);
+        // send type3 message to server:
+        if (stream) {
+            $(request(type3options));
+        } else {
+            request(type3options, $);
+        }
+    }
 
-			// build type3 request:
-			var type3options = {
-				headers: {
-					'Connection': 'Close',
-					'Authorization': type3msg
-				},
-				followRedirect: false,
-				forever: true
-			};
-			type3options.url = reqOptions.url;
+    makeType1Request(function (error, response) {
+        if (error) {
+            return callback(error);
+        }
 
-			// pass along other options:
-			type3options.headers = _.extend(type3options.headers, reqOptions.headers);
-			type3options = _.extend(type3options, _.omit(reqOptions, 'headers'));
+        if (!response.headers['www-authenticate']) {
+            return callback(new Error('www-authenticate not found on response of second request'));
+        }
 
-			// send type3 message to server:
-			if (stream) {
-				$(request(type3options));
-			} else {
-				request(type3options, $);
-			}
-		}
-	], callback);
+        makeType3Request(response.headers['www-authenticate'], callback);
+    });
 }
 
-exports.stream = function(options, callback) {
-	makeRequest(true, options, callback);
-}
+exports.stream = function (options, callback) {
+    makeRequest(true, options, callback);
+};
 
-exports.fetch = function(options, callback) {
-	makeRequest(false, options, callback);
-}
+exports.fetch = function (options, callback) {
+    makeRequest(false, options, callback);
+};
 
 exports.ntlm = ntlm; //if you want to use the NTML functions yourself
--- test/index.js
+++ test/index.js
@@ -1,24 +1,24 @@
 var ntlm = require('request-simple-ntlm');
 
 var options = {
-	url: <URL>,
-	username: <Username>,
-	password: <Password>,
-	request: {
-		headers: {
-			'Accept': 'application/json'
-		}
-	}
+    url: "<url>",
+    username: "<username>",
+    password: "<password>",
+    request: {
+        headers: {
+            'Accept': 'application/json'
+        }
+    }
 };
 
-ntlm.fetch(options, function(err, resp, body) {
-	console.log(err, body);
-}, function(err) {
-	 console.log('Error is : ', err);
+ntlm.fetch(options, function (err, resp, body) {
+    console.log(err, body);
+}, function (err) {
+    console.log('Error is : ', err);
 });
 
-ntlm.stream(options, function(request) {
-	request.pipe(process.stdout);
-}, function(err) {
-	 console.log('Error is : ', err);
+ntlm.stream(options, function (request) {
+    request.pipe(process.stdout);
+}, function (err) {
+    console.log('Error is : ', err);
 });
